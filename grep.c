ba9e3389497339ac4e40a4910ce28346b915fce2
falcon <pulkitsapra97@gmail.com>
2017-08-20 16:15:17 +0530

    with ?

--- grep.c
+++ grep.c
@@ -7,71 +7,100 @@
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
-int main()
-{
-	FILE* fp;
-	char pat[100];
-	char pattern[100];
-	char cpy[100];
-	char *ret;
-	char c[100];
-	fp = fopen("inpt.txt", "r");
-	printf("%s","Enter pattern to be matched : ");
-	scanf("%s",pat);
-	int flag=0;
-	int lineno=0;
-	int found=0;
-	int k;
-	while(1)
-	{
 
-		fgets(c,1024,fp);
-		lineno++;
-		int flag=0;
+
+/////////////////////////////////////////////////////////////////////////////
+//*******   function to print the highlighted matched characters       ******
+
+void print(int lineno, char c[100],char pat[100],int i){
+	int m=0;
+	int t=0;
+	printf("%d : ",lineno);
+	while(c[m]!='\n'){
+		if(m==i){
+			while((c[m]==pat[t] || pat[t]=='?') && t<strlen(pat)){
+				printf(ANSI_COLOR_GREEN "%c",c[m] );
+				t++;
+				m++;
+			}
+		}
+		else{
+			printf(ANSI_COLOR_RESET "%c",c[m] );
+			m++;
+		}			
+
+	}
+	printf("\n");
+
+}
+///////////////////////////////////////////////////////////////////////////
+//*****************         function to get matching  *********************
+
+int match(int lineno, char pat[100],char c[100]){
+	int found=0;
+	int flag=0;
+	int k;
 		for(int i=0;i<strlen(c);++i){
-			if(c[i]==pat[0]){
+			if(c[i]==pat[0] || pat[0]=='?'){
 				flag=0;
 				int j=i;
 				for(k=0; k<strlen(pat) && j<strlen(c);++k,++j){
+					if(pat[k]=='?')
+						continue;
 					if(c[j]!=pat[k]){
 						flag=1;
 						break;
 					}
 				}
-				if(j==strlen(c)&&k<strlen(pat)){
+				if(j==strlen(c)&&k<strlen(pat))
 					flag=1;
-				}
-
 				if(flag==0){
-					int m=0;
-					int t=0;
-					printf("%d : ",lineno);
-					while(c[m]!='\n'){
-						if(m==i){
-							while(c[m]==pat[t] && t<strlen(pat)){
-								printf(ANSI_COLOR_YELLOW "%c",c[m] );
-								t++;
-								m++;
-							}
-						}
-						else{
-							printf(ANSI_COLOR_RESET "%c",c[m] );
-							m++;
-						}					
-					}
-					printf("\n");
-					found =1;
+					found=1;
+					print(lineno,c,pat,i);
 				}
 			}
-		} 
-
-		if(feof(fp)){
-			break;
 		}
+		return found;
+}
+///////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+	FILE* fp;
+	char pat[100];
+	char pattern[100];
+	int found=0,foundq=0;
+	int c_found=0;
+	char c[100];
+	fp = fopen("inpt.txt", "r");
+	printf("%s","Enter pattern to be matched : ");
+	scanf("%s",pat);
+	int k=0;
+	int q =0;
+	for(int i=0;i<strlen(pat);++i){
+		if(pat[i]!='?')
+			pattern[k++]=pat[i];
 	}
-	if(!found)
+	pattern[k]='\0';
+	if(strcmp(pat,pattern)!=0)
+		q=1;
+	int flag=0;
+	int lineno=0;	
+	while(1){
+		fgets(c,1024,fp);
+		lineno++;
+		found =match(lineno,pat,c);
+		if(q==1)
+			foundq =match(lineno,pattern,c); 
+
+		if(found || foundq)
+			c_found=1;
+
+		if(feof(fp))
+			break;
+	}
+	if(!c_found)
 		printf("%s\n","Not Found any Match");
-
-
+	fclose(fp);
 	return 0;
 }
